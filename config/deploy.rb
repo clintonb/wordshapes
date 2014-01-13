c21df5db7543f6609c89b7ebccd0d6bd4992d3dc
Clinton Blackburn <clinton.blackburn@gmail.com>
2014-01-12 21:11:25 -0500

    Removed submodules

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,40 @@
-set :application, 'wordshapes'
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
-set :scm, :git
-set :git_enable_submodules, 1
-set :repository, 'ssh://git@git.assembla.com/wordshapes.git'
-set :branch, 'master'
-set :branch, tag if exists?(:tag) # Deploy tag if it is passed on command line
-set :deploy_via, :remote_cache
-set :scm_verbose, true
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-set :user, 'ec2-user'
-set :use_sudo, false
-set :ssh_options, { :forward_agent => true }
+# set :deploy_to, '/var/www/my_app'
+# set :scm, :git
 
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
 
-role :web, 'wordshapes.clintonblackburn.com'
-role :app, 'wordshapes.clintonblackburn.com'
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-after 'deploy:restart', 'deploy:cleanup'
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
